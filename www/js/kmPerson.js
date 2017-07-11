10a55d723db2a6c66df08d7df7518d5661ced94c
Alonso Salcido <alonsosalcido13@gmail.com>
2017-07-10 19:03:18 -0600

    kms run by me

---
+++ www/js/kmPerson.js
@@ -0,0 +1,27 @@
+const nombre = "Alonso";
+const dias = [
+    "lunes",
+    "martes",
+    "miercoles",
+    "jueves",
+    "viernes",
+    "sabado",
+    "domingo"
+]
+
+function correr(){
+    const min = 5
+    const max = 15
+    return Math.round(Math.random() * (max-min) + min);
+}
+
+let totalKms = 0
+const length = dias.length;
+for(let i = 0; i<length; i++){
+    const kms = correr();
+    totalKms += kms
+    console.log(`El dia ${dias[i]} ${nombre} corrió ${kms}kms`);
+}
+
+const promedioKms = totalKms / length;
+console.log(`En promedio, ${nombre} corrió ${promedioKms.toFixed(2)} kms`)
